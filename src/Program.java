745e636e9ce6faf34334a4cb6594ff60c89d5d8e
Arthur Alberto <arturalberto7@gmail.com>
2025-12-09 17:48:20 -0300

    Criando Seller

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,13 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args){
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+        Seller seller = new Seller(21, obj, 3000.00, new Date(), "bob@gmail.com", "bob");
+        System.out.println(seller);
     }
 }
